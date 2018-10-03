d8e48e25e9889e8b8d23f02b8102fe45b81cc460
Ken Ng <ng.ken84@gmail.com>
2018-10-02 23:13:56 -0700

    Set up View and View Controller connection

--- app/src/main/java/joker/persona/ngrocken/androidfirebaseloginexample/MainActivity.java
+++ app/src/main/java/joker/persona/ngrocken/androidfirebaseloginexample/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailEdtTxt;
+    private EditText passwordEdtTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpView() {
+        emailEdtTxt = findViewById(R.id.main_email_edit);
+        passwordEdtTxt = findViewById(R.id.main_pass_edit);
+
+        findViewById(R.id.main_login_btn).setOnClickListener(this);
+
         findViewById(R.id.main_signup_link).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/joker/persona/ngrocken/androidfirebaseloginexample/RegisterActivity.java
+++ app/src/main/java/joker/persona/ngrocken/androidfirebaseloginexample/RegisterActivity.java
@@ -2,12 +2,33 @@ package joker.persona.ngrocken.androidfirebaseloginexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText userNameEdt;
+    private EditText emailEdt;
+    private EditText passEdt;
+    private EditText confirmEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    private void setUpView() {
+        userNameEdt = findViewById(R.id.regis_username_edit);
+        emailEdt = findViewById(R.id.regis_email_edit);
+        passEdt = findViewById(R.id.regis_password_edit);
+        confirmEdt = findViewById(R.id.regis_confirm_edit);
+
+        findViewById(R.id.regis_regis_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
