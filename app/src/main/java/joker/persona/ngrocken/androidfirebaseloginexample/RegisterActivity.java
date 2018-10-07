a30e500a167c87536ac31a8e6bb7cea3ba0fb14e
Ken Ng <ng.ken84@gmail.com>
2018-10-07 10:52:45 -0700

    Implement Registration Page with simple toast message

--- app/src/main/java/joker/persona/ngrocken/androidfirebaseloginexample/RegisterActivity.java
+++ app/src/main/java/joker/persona/ngrocken/androidfirebaseloginexample/RegisterActivity.java
@@ -1,9 +1,17 @@
 package joker.persona.ngrocken.androidfirebaseloginexample;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -12,10 +20,16 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText passEdt;
     private EditText confirmEdt;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        setUpView();
     }
 
     private void setUpView() {
@@ -29,6 +43,40 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View view) {
+        String password = passEdt.getText().toString().trim();
+        String confirm = confirmEdt.getText().toString().trim();
+        String email = emailEdt.getText().toString().trim();
+        String username = userNameEdt.getText().toString().trim();
 
+        boolean hasError = false;
+        if(TextUtils.isEmpty(username)) {
+            userNameEdt.setError("Please enter a user .");
+            hasError = true;
+        }
+        if (TextUtils.isEmpty(email)) {
+            emailEdt.setError("Please enter an email.");
+            hasError = true;
+        }
+        if(TextUtils.isEmpty(password)) {
+            passEdt.setError("Please enter a password.");
+            hasError = true;
+        }
+        if(password != null && !password.equals(confirm)) {
+            confirmEdt.setError("Your passwords do not match.");
+            hasError = true;
+        }
+        
+        if(hasError) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Account Created", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
